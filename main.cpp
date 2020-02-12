d4ccc868fd6e0f42d47c10eb1816935b1f1ce663
gergopc <gergoesjanka@gmail.com>
2020-02-12 07:02:16 +0200

    Minimalis Osszeg megvan

--- main.cpp
+++ main.cpp
@@ -1,246 +1,67 @@
 #include <bits/stdc++.h>
 
+#define MAX 102
+
 using namespace std;
 
-void be(int &n, int &iCsp, char strFa[]) {
-    ifstream in("file.in");
-    in >> strFa;
+int n, k;
+int iMatrix[MAX][MAX];
+int iVerem[MAX];
+int iMin=9999;
+
+void beolvas(int &n, int iMatrix[][MAX]) {
+    ifstream fin("file.in");
+    fin >> n;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            fin >> iMatrix[i][j];
+        }
+    }
 }
 
-void direkt(int csp, char strFa[]) {
-    cout << "Direkt leszarmazottak: ";
-
-    char strCsp[100];
-    char strKeres[100], strKi[100] = "";
-    int k = 0, ok = 1, iDb = 0;
-    bool bOk = 0;
-    itoa(csp, strCsp, 10);
-
-    strcpy(strKeres, strstr(strFa, strCsp));
-
-    for (int i = 0; strKeres[i] != '\0' && ok == 1; i++) {
-        if (strKeres[i] == '(') k++;
-        if (strKeres[i] == ')') {
-            k--;
-            if (k == 0)
-                ok = 0;
-        }
-        if (strKeres[i] == ',' && k == 0)
-            ok = 0;
-        if (k == 1 and strKeres[i] > '0' and strKeres[i] < '9') {
-            strKi[iDb++] = strKeres[i];
-            strKi[iDb] = '\0';
-            bOk = 1;
-        } else if (bOk == 1) {
+bool ellenoriz(int n, int k) {
+    bool bOk = 1;
+    for (int i = k - 1; i >= 1 && bOk == 1; --i) {
+        if (iVerem[k] == iVerem[i])
             bOk = 0;
-            cout << strKi << ' ';
-            iDb = 0;
-            strKi[0] = '\0';
-        }
     }
-
-    cout << endl;
+    return bOk;
 }
 
-int magassag(char strFa[]) {
-    int iBalZarojelek = 0;
-    int iMax = 0;
-
-    for (int i = 0; strFa[i] = '\0'; i++) {
-        if (strFa[i] == '(') {
-            iBalZarojelek++;
-            if (iBalZarojelek > iMax) iMax = iBalZarojelek;
-        } else if (strFa[i] == ')') {
-            iBalZarojelek--;
-        }
-    }
-    return iMax;
+bool megoldas(int n, int k) {
+    return k == n;
 }
 
-void osszesLeszarmazott(int iCsp, char strFa[]) {
-    cout << "Osszes leszarmazott: ";
-
-    char strCsp[100];
-    char strKeres[100], strKi[100] = "";
-    int k = 0, ok = 1, iDb = 0;
-    bool bOk = 0;
-    itoa(iCsp, strCsp, 10);
-
-    strcpy(strKeres, strstr(strFa, strCsp));
-
-    for (int i = 0; strKeres[i] != '\0' && ok == 1; i++) {
-        if (strKeres[i] == '(') k++;
-        if (strKeres[i] == ')') {
-            k--;
-            if (k == 0)
-                ok = 0;
-        }
-        if (strKeres[i] == ',' && k == 0)
-            ok = 0;
-        if (k >= 1 and strKeres[i] > '0' and strKeres[i] < '9') {
-            strKi[iDb++] = strKeres[i];
-            strKi[iDb] = '\0';
-            bOk = 1;
-        } else if (bOk == 1) {
-            bOk = 0;
-            cout << strKi << ' ';
-            iDb = 0;
-            strKi[0] = '\0';
-        }
+void feldolgozMegoldas(int n) {
+    int iOsszeg = 0;
+    for (int i = 1; i <= n; ++i) {
+        iOsszeg += iMatrix[i][iVerem[i]];
     }
-    cout << endl;
-
+    iMin = min(iMin, iOsszeg);
 }
 
-void levelek(char strFa[]) {
-    cout << "Levelek: ";
-    char strKi[256] = "";
-    int iHossz = strlen(strFa), iDb = 0;
-    bool bOk = 0;
-    for (int i = 0; strFa[i] != '\0'; i++) {
-
-        if (strFa[i] > '0' and strFa[i] < '9') {
-            strKi[iDb++] = strFa[i];
-            strKi[iDb] = '\0';
-            bOk = 1;
-        } else if (bOk == 1) {
-            bOk = 0;
-            if (strFa[i] != '(')
-                cout << strKi << ' ';
-            iDb = 0;
-            strKi[0] = '\0';
-        }
-    }
-    cout << endl;
-}
-
-void preorder(char strFa[]) {
-    char strKi[256];
-    int iDb = 0;
-    bool bOk = 0;
-    cout << "Preorder bejaras: ";
-    for (int i = 0; strFa[i] != '\0'; ++i) {
-        if (strFa[i] > '0' and strFa[i] < '9') {
-            strKi[iDb++] = strFa[i];
-            strKi[iDb] = '\0';
-            bOk = 1;
-        } else if (bOk == 1) {
-            bOk = 0;
-            cout << strKi << ' ';
-            iDb = 0;
-            strKi[0] = '\0';
-        }
-    }
-    cout << endl;
-}
-
-void kiirSzam(char str[]) {
-    for (int i = 0; str[i] >= '0' && str[i] <= '9'; ++i) {
-        cout << str[i];
-    }
-    cout << ' ';
-}
-
-/*
-void postorder(char strFa[]) {   //BJGY
-    bool bZarojel = 0, bVesszo = 0, bOk = 0;
-    int iSzint=0;
-    //bal
-    bOk = 1;
-    for (int i = 0; strFa[i] != '\0' && bVesszo == 0 && bOk == 1; i++) {
-        if (strFa[i] == '(') {
-            postorder(strFa + i + 1);
-            bOk = 0;
-        } else if (strFa[i] == ',') bVesszo = 1;
+void backtrack(int n, int k) {
+    k = 1;
+    while (k > 0) {
+        if (iVerem[k] < n) {
+            iVerem[k]++;
+            if (ellenoriz(n, k)) {
+                if (megoldas(n, k)) {
+                    feldolgozMegoldas(n);
+                } else {
+                    k++;
+                    iVerem[k] = 0;
+                }
+            }
+        } else k--;
     }
 
-    //jobb
-    bOk = 1;
-    for (int i = 0; strFa[i] != '\0' && bZarojel == 0 && bOk == 1; i++) {
-        if (strFa[i] == ',') {
-            postorder(strFa + i + 1);
-            bOk = 0;
-        } else if (strFa[i] == '(') bZarojel = 1;
-    }
-    //gyoker
-    kiirSzam(strFa);
-}
-*/
 
-void postorder(char strFa[]) {   //BJGY
-    bool bZarojel = 0, bOk = 0;
-    int iSzint = 0, iBal = -1, iJobb = -1, iVesszo = 0;
-    //bal
-    bOk = 1;
-    for (int i = 0; strFa[i] != '\0' && iSzint >= 0; i++) {
-        if (strFa[i] >= '0' && strFa[i] <= '9') {
-            if (iVesszo == 0 && iSzint == 1 && iBal == -1 && bOk == 1)
-                iBal = i;
-            else if (iVesszo == 1 && iSzint == 1 && iJobb == -1 && bOk == 1)
-                iJobb = i;
-        } else if (strFa[i] == ',') {
-            iVesszo = 1;
-            if (iSzint == 0)
-                bOk = 0;
-        } else if (strFa[i] == '(') {
-            iSzint++;
-        } else if (strFa[i] == ')')
-            iSzint--;
-    }
-    //bal
-    if (iBal >= 0)
-        postorder(strFa + iBal);
-    //jobb
-    if (iJobb >= 0)
-        postorder(strFa + iJobb);
-    //gyoker
-    kiirSzam(strFa);
-}
-
-void inorder(char strFa[]) {   //BGYJ
-    bool bZarojel = 0, bOk = 0;
-    int iSzint = 0, iBal = -1, iJobb = -1, iVesszo = 0;
-    //bal
-    bOk = 1;
-    for (int i = 0; strFa[i] != '\0' && iSzint >= 0; i++) {
-        if (strFa[i] >= '0' && strFa[i] <= '9') {
-            if (iVesszo == 0 && iSzint == 1 && iBal == -1 && bOk == 1)
-                iBal = i;
-            else if (iVesszo == 1 && iSzint == 1 && iJobb == -1 && bOk == 1)
-                iJobb = i;
-        } else if (strFa[i] == ',') {
-            iVesszo = 1;
-            if (iSzint == 0)
-                bOk = 0;
-        } else if (strFa[i] == '(') {
-            iSzint++;
-        } else if (strFa[i] == ')')
-            iSzint--;
-    }
-    //bal
-    if (iBal >= 0)
-        inorder(strFa + iBal);
-    //gyoker
-    kiirSzam(strFa);
-    //jobb
-    if (iJobb >= 0) {
-        inorder(strFa + iJobb);
-    }
 }
 
 int main() {
-    int n, csp;
-    char fa[100];
-    be(n, csp, fa);
-    cin >> csp;
-    direkt(csp, fa);
-    //cout<<"Fa magassaga: "<<magassag(fa);
-    osszesLeszarmazott(csp, fa);
-    levelek(fa);
-    preorder(fa);
-    cout << "Postorder: ";
-    postorder(fa);
-    cout << endl;
-    cout << "Inorder: ";
-    inorder(fa);
+
+    beolvas(n, iMatrix);
+    backtrack(n, k);
+    cout << iMin;
 }
