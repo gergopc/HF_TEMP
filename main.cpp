0fe4ceb3822fdaa805b97f08994969b15e6364f0
gergopc <gergoesjanka@gmail.com>
2020-03-26 13:03:32 +0200

    polinom

--- main.cpp
+++ main.cpp
@@ -1,100 +1,179 @@
 #include <bits/stdc++.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
 struct Lista {
-    int ertek;
+    int eh;
+    int fsz;
     Lista *kovetkezo;
-    Lista *elozo;
 };
 
-Lista *lista, *lOrszem;
+Lista *f1, *f2, *lOrszem1, *lOrszem2, *lOsszeg, *lSzorzat;
 
-int n;
+int n, m;
 
-Lista *ujElem(int x) {
+Lista *ujElem(int eh, int fsz) {
     Lista *lUj = new Lista;
-    lUj->ertek = x;
+    lUj->eh = eh;
     lUj->kovetkezo = nullptr;
-    lUj->elozo = nullptr;
+    lUj->fsz = fsz;
     return lUj;
 }
 
-ifstream fin("file.in");
+ifstream finPOL1("POL1.TXT");
+ifstream finPOL2("POL2.TXT");
+ofstream foutPOL("POL.TXT");
+
+void beszur(Lista *&lista, Lista *&lOrszem, int eh, int fsz) {
+    if (lista == nullptr) {
+        lista = ujElem(eh, fsz);
+        lOrszem = lista;
+    } else {
+        lOrszem->kovetkezo = ujElem(eh, fsz);
+        lOrszem = lOrszem->kovetkezo;
+    }
+}
 
 void beszur(Lista *&lista, Lista *&lOrszem, int x) {
-    Lista *lUj = ujElem(x);
-    Lista *lSeged = lista;
+    Lista *lUj = ujElem(0, x);
     if (lista == nullptr) {
         lista = lUj;
-        lista->kovetkezo = lista;
-        lista->elozo = lista;
         lOrszem = lista;
     } else {
         lOrszem->kovetkezo = lUj;
-        lUj->elozo = lOrszem;
         lOrszem = lUj;
-        lOrszem->kovetkezo = lista;
     }
 }
 
-void beolvas() {
-    fin >> n;
+void beszurOsszead(Lista *&lista, Lista *&lOrszem, int eh, int fsz) {
+
+
+    if (lista == nullptr)
+        beszur(lista, lOrszem, eh, fsz);
+    else {
+        Lista *lElotte = new Lista;
+        lElotte->kovetkezo = lista;
+        Lista *lUtana = lista;
+
+        while (lUtana != nullptr && lUtana->fsz >= fsz) {
+            lElotte = lElotte->kovetkezo;
+            lUtana = lUtana->kovetkezo;
+        }
+
+        if (lElotte->fsz == fsz) {
+            lElotte->eh += eh;
+        } else {
+            Lista *lUj = ujElem(eh, fsz);
+            lElotte->kovetkezo = lUj;
+            lUj->kovetkezo = lUtana;
+            if (lUtana == lista)
+                lista = lUj;
+        }
+
+
+    }
+}
+
+void beolvas(Lista *&lista, Lista *&lOrszem, ifstream &fin) {
     int x;
+    Lista *lSeged2 = nullptr;
+    Lista *lSeged = nullptr;
+    Lista *lOrszemSeged;
+    fin >> n;
     for (int i = 0; i < n; ++i) {
         fin >> x;
-        beszur(lista, lOrszem, x);
+        beszur(lSeged, lOrszemSeged, x);
     }
-}
-
-void kiir(Lista *lista) {
-    Lista *lSeged = lista;
-    do {
-        cout << lSeged->ertek << ' ';
-        lSeged = lSeged->kovetkezo;
-        if (lSeged->elozo == lSeged)
-            cout << "Serult mutato ";
-    } while (lSeged != lista);
-}
-
-bool randomBool() {
-    static auto gen = std::bind(std::uniform_int_distribution<>(0, 1), std::default_random_engine());
-    return gen();
-}
-
-void elront(Lista *&lista) {
-    bool bRandom = randomBool();
-    Lista *lSeged = lista;
-    do {
-        bRandom = randomBool();
-        if (bRandom)
-            lSeged->elozo = lSeged;
-        lSeged = lSeged->kovetkezo;
-    } while (lSeged != lista);
-
-}
-
-void megjavit(Lista *&lista) {
-    Lista *lSeged1, *lSeged2;
-    lista->elozo = nullptr;
-    lSeged1 = lista;
-    lSeged2 = lista->kovetkezo;
-    do {
-        lSeged2->elozo = lSeged1;
-        lSeged1 = lSeged1->kovetkezo;
+    lSeged2 = lSeged;
+    for (int i = 0; i < n; ++i) {
+        fin >> x;
+        lSeged2->eh = x;
         lSeged2 = lSeged2->kovetkezo;
+    }
+    lSeged2 = lSeged;
+    while (lSeged2 != nullptr) {
+        beszurOsszead(lista, lOrszem, lSeged2->eh, lSeged2->fsz);
+        lSeged2 = lSeged2->kovetkezo;
+    }
 
-    } while (lSeged1 != lista);
+}
+
+
+Lista *osszead(Lista *f1, Lista *f2) {
+    //int iLegnagyobbFok = 0;
+    int iEh, iFok;
+    Lista *lOsszeg = nullptr;
+    Lista *lOsszegOrszem = nullptr;
+    Lista *lSeged1 = f1;
+    Lista *lSeged2 = f2;
+    if (f1 == nullptr)
+        return f2;
+    else if (f2 == nullptr)
+        return f1;
+    while (lSeged1 != nullptr && lSeged2 != nullptr) {
+        if (lSeged1->fsz > lSeged2->fsz) {
+            beszur(lOsszeg, lOsszegOrszem, lSeged1->eh, lSeged1->fsz);
+            lSeged1 = lSeged1->kovetkezo;
+        } else if (lSeged2->fsz > lSeged1->fsz) {
+            beszur(lOsszeg, lOsszegOrszem, lSeged2->eh, lSeged2->fsz);
+            lSeged2 = lSeged2->kovetkezo;
+        } else {
+            beszur(lOsszeg, lOsszegOrszem, lSeged2->eh + lSeged1->eh, lSeged2->fsz);
+            lSeged2 = lSeged2->kovetkezo;
+            lSeged1 = lSeged1->kovetkezo;
+        }
+    }
+    while (lSeged1 != nullptr) {
+        beszur(lOsszeg, lOsszegOrszem, lSeged1->eh, lSeged1->fsz);
+        lSeged1 = lSeged1->kovetkezo;
+    }
+    while (lSeged2 != nullptr) {
+        beszur(lOsszeg, lOsszegOrszem, lSeged2->eh, lSeged2->fsz);
+        lSeged2 = lSeged2->kovetkezo;
+    }
+    return lOsszeg;
+}
+
+void kiirat(Lista *lista) {
+    Lista *lSeged = lista;
+    while (lSeged != nullptr) {
+        foutPOL << lSeged->fsz << ' ';
+        lSeged = lSeged->kovetkezo;
+    }
+    foutPOL << endl;
+    lSeged = lista;
+    while (lSeged != nullptr) {
+        foutPOL << lSeged->eh << ' ';
+        lSeged = lSeged->kovetkezo;
+    }
+    foutPOL << endl;
+}
+
+Lista *szoroz(Lista *f1, Lista *f2) {
+    Lista *lSzorzat = nullptr, *lSzorzatOrszem = nullptr;
+    Lista *lSeged1 = f1;
+    Lista *lSeged2 = f2;
+    while (lSeged1 != nullptr) {
+        lSeged2 = f2;
+        while (lSeged2 != nullptr) {
+            if (lSeged2->eh * lSeged1->eh != 0) {
+                beszurOsszead(lSzorzat, lSzorzatOrszem, lSeged2->eh * lSeged1->eh, lSeged2->fsz + lSeged1->fsz);
+            }
+            lSeged2 = lSeged2->kovetkezo;
+        }
+        lSeged1 = lSeged1->kovetkezo;
+    }
+
+    return lSzorzat;
 }
 
 int main() {
-
-    beolvas();
-    kiir(lista);
-    cout << endl;
-    elront(lista);
-    kiir(lista);
-    cout << endl;
-    megjavit(lista);
-    kiir(lista);
+    beolvas(f1, lOrszem1, finPOL1);
+    beolvas(f2, lOrszem2, finPOL2);
+    lOsszeg = osszead(f1, f2);
+    lSzorzat = szoroz(f1, f2);
+    kiirat(lOsszeg);
+    kiirat(lSzorzat);
 }
